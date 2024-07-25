623047cca42747acbafe6df0111d6923aa096e9f
vaishnavi thakare <thakarevaishnavi10@gmail.com>
2024-07-25 18:31:40 +0530

    Add files via upload

---
+++ Object_Class/ToString_Method.java
@@ -0,0 +1,19 @@
+//program 1
+
+class ToString_Method
+{
+	public static void main(String[] args) 
+	{
+		String name="vaish";
+		String str= new String(name);
+		System.out.println(str);
+		System.out.println(str.toString());
+
+	    ToString_Method obj= new ToString_Method();
+		System.out.println(obj);
+	}
+}
+
+// vaish
+// vaish
+// ToString_Method@372f7a8d
