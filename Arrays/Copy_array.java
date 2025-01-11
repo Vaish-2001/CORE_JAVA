fdc6122b1418cb5335ea46dd2fa83356618420e2
vaishnavi thakare <thakarevaishnavi10@gmail.com>
2025-01-11 22:20:44 +0530

    Add files via upload

---
+++ Arrays/Copy_array.java
@@ -0,0 +1,17 @@
+class Copy_array
+{
+	public static void main(String[] args) {
+		int[] arr={1,2,3,4,5};
+		int [] copy= new int[arr.length];
+
+		for(int i =0;i<arr.length;i++)
+		{
+			copy[i]=arr[i];
+		}
+		for (int i = 0; i < copy.length; i++) {
+            System.out.print(copy[i] + " ");
+		}
+	}
+}
+
+//1 2 3 4 5
