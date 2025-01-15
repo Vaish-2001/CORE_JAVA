ee7a33426ee809b9898319c4a4434a3ba6a8e00c
vaishnavi thakare <thakarevaishnavi10@gmail.com>
2025-01-15 22:43:44 +0530

    Add files via upload

---
+++ String/IsEmplty_method.java
@@ -0,0 +1,14 @@
+class IsEmplty_method
+{
+	public static void main(String[] args)
+	{
+		String str1= new String();
+		System.out.println(str1.isEmpty());
+
+		String str2= new String("hello");
+		System.out.println(str2.isEmpty());
+	}
+}
+
+// true
+// false
